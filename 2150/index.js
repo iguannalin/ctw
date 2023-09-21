54a7be4c140a8cb828601e3b0169606bdd1ee8d8
seasons986 <annalin12345@gmail.com>
2023-09-21 09:58:28 -0400

    could prob put index.js into the html text variable as well but that seems a bit unnecessarily extreme

--- 2150/index.js
+++ 2150/index.js
@@ -8,60 +8,23 @@ window.addEventListener("load", () => {
   const heading = document.getElementById("heading");
   const link = document.getElementById("link");
   const img = document.getElementById("img");
-  const container = document.getElementById("container");
 
   function displayItem(item) {
-    console.log(item);
-    // let total = Number(sessionStorage.getItem("total"));
-    // let page = sessionStorage.getItem("page");
-    // page++;
     let total = heading.dataset.total;
     if (total != 2150 && total >= 500) {
-      heading.innerHTML = `you ran out of ${item.name.split(" ")[0]}, so you bought some ${item.name}<br><br>you have $${Math.round(total * 100) / 100} left.`
+      heading.innerHTML = `you ran out, so you bought some ${item.name}<br><br>you have $${Math.round(total * 100) / 100} left.`
       img.src = item.imageThumbnail;
       img.width = "200";
-      // link.href = `${page}.html`;
       link.innerHTML = "continue shopping";
-    } 
-    // else {
-      //   heading.innerHTML = `you now have $${total}.`
-      //   container.removeChild(img);
-      //   container.removeChild(link);
-      // }
-      // sessionStorage.setItem("total", total);
-      // sessionStorage.setItem("page", page);
-      total -= item.regularPrice;
+    }
+    total -= item.regularPrice;
       
     link.onclick = () => {
-
-      const text = `<!doctypehtml><title>2150</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><script src=https://iguannalin.github.io/ctw/2150/index.js></script><div id=container><h1 id=heading data-total="${total}"></h1><img id=img> <a id=link>let’s go shopping</a></div>`;
-
-
-// const byteCharacters = atob(base64ImageData.substr(`data:${contentType};base64,`.length));
-// const byteArrays = [];
-
-// for (let offset = 0; offset < byteCharacters.length; offset += 1024) {
-//     const slice = byteCharacters.slice(offset, offset + 1024);
-
-//     const byteNumbers = new Array(slice.length);
-//     for (let i = 0; i < slice.length; i++) {
-//         byteNumbers[i] = slice.charCodeAt(i);
-//     }
-
-//     const byteArray = new Uint8Array(byteNumbers);
-
-//     byteArrays.push(byteArray);
-// }
-// const blob = new Blob(byteArrays, {type: contentType});
-var blob = new Blob([text], {type: "text/html"});
-const blobUrl = URL.createObjectURL(blob);
-
-window.open(blobUrl, '_blank');
-window.URL.revokeObjectURL(blobUrl)
-      // const opened = window.open(`data:text/html,<div style="line-height:1em;word-wrap:break-word;"id=d><script>for(i=0;1e4>i;i++)d.innerHTML+=Math.random()<.5&&"\u29F8"||"\u29F9"</script>`);
-      // const opened = window.open("");
-      // opened.document.writeln(`<!doctypehtml><title>2150</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><script src=https://iguannalin.github.io/ctw/2150/index.js></script><div id=container><h1 id=heading data-total="${total}"></h1><img id=img> <a id=link>let’s go shopping</a></div>`);
-      // console.log({opened});
+      const text = `<!doctypehtml><title>2150</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><script src=https://iguannalin.github.io/ctw/2150/index.js></script><div id=container><h1 id=heading data-total="${total}"></h1><img id=img> <button id=link>let’s go shopping</button></div>`;
+      const blob = new Blob([text], {type: "text/html"});
+      const blobUrl = URL.createObjectURL(blob);
+      window.open(blobUrl, '_blank');
+      window.URL.revokeObjectURL(blobUrl);
     }
   }
 
