3cc85a20bc3e6cc6d27c96bd85ec0066028ac101
seasons986 <annalin12345@gmail.com>
2023-09-27 04:04:58 -0400

    obssesseD

--- manifesto/index.js
+++ manifesto/index.js
@@ -1,30 +1,45 @@
 window.addEventListener("load", () => {
   const container = document.getElementById("container");
   const center = container.dataset.coord;
-  console.log({center});
+  const whalf = window.innerWidth/2;
+  const hhalf = window.innerHeight/2;
   const coordsX = [0,1,0,-1];
   const coordsY = [1,0,-1,0];
-  
-  function assign(text) {
-    coordsX.forEach((x) => {
-      coordsY.forEach((y) => {
-        const coord = `${x++},${y}`;
-        if (d[coord]) {}
-      })
+
+  function display(rule, x, y, isCenter = false) {
+    const link = document.createElement("a");
+    if (isCenter) {
+      link.style.left = whalf +"px";
+      link.style.top = hhalf+"px";
+      link.classList = "center";
+    } else {
+      link.style.left = whalf+(x*250)+"px";
+      link.style.top = hhalf+(y*250)+"px";
+    }
+    link.innerText = rule;
+    link.onclick = () => {
+      const text = `<!doctypehtml><title>2150</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><script src=https://iguannalin.github.io/ctw/2150/index.js></script><div id=container><h1 id=heading data-total="${total}"></h1><img id=img> <button id=link>let’s go shopping</button></div>`;
+      const blob = new Blob([text], {type: "text/html"});
+      const blobUrl = URL.createObjectURL(blob);
+      window.open(blobUrl, '_blank');
+      window.URL.revokeObjectURL(blobUrl);
+    }
+    document.body.appendChild(link);
+  }
+
+  function assign(rules) {
+    if (rules[center]) display(rules[center], 0, 0, true);
+    coordsX.forEach((x,index) => {
+      const cCoord = center.split(",");
+      const coord = `${+cCoord[0]+x},${+cCoord[1]+coordsY[index]}`;
+      if (rules[coord]) {
+        display(rules[coord], x, coordsY[index]);
+      }
     })
     
-      const link = document.createElement("a");
-      link.innerText = t;
-      link.onclick = () => {
-        const text = `<!doctypehtml><title>2150</title><meta charset=utf-8><meta content="width=device-width,initial-scale=1"name=viewport><script src=https://iguannalin.github.io/ctw/2150/index.js></script><div id=container><h1 id=heading data-total="${total}"></h1><img id=img> <button id=link>let’s go shopping</button></div>`;
-        const blob = new Blob([text], {type: "text/html"});
-        const blobUrl = URL.createObjectURL(blob);
-        window.open(blobUrl, '_blank');
-        window.URL.revokeObjectURL(blobUrl);
-      }
   }
 
   fetch("manifesto.json").then((r) => r.json()).then((d) => {
-    console.log(d);
+    assign(d);
   })
 });
