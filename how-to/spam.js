b88dc878f6a1075a092ee6dd530899a6b0babd58
seasons986 <annalin12345@gmail.com>
2023-09-28 12:17:54 -0400

    debugging

--- how-to/spam.js
+++ how-to/spam.js
@@ -10,7 +10,6 @@ window.addEventListener("load", () => {
   const palettes = ["fde725","a0da39","4ac16d","1fa187","277f8e","365c8d","46327e","440154","f0f921","febd2a","f48849","db5c68","b83289","8b0aa5","5302a3","0d0887"];
 
   function showContent(value) {
-    console.log({key, value});
     if (!key || !value) return;
     container.style.backgroundColor = "#"+palettes[getRandomInt(0, palettes.length)];
     const div = document.createElement("div");
@@ -24,6 +23,7 @@ window.addEventListener("load", () => {
   }
 
   fetch("https://iguannalin.github.io/ctw/how-to/things.json").then((r) => r.json()).then((d) => {
+    console.log({key, d, d[key]});
     showContent(d[key]);
   })
 });
