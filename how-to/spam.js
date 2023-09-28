911a9c7bfb39617fd7b88cab4f578558540aabfe
seasons986 <annalin12345@gmail.com>
2023-09-28 11:59:22 -0400

    fixed weird html import issue

--- how-to/spam.js
+++ how-to/spam.js
@@ -1,15 +1,21 @@
 window.addEventListener("load", () => {
   const container = document.getElementById("container");
   const key = container.dataset.key;
-  const value = container.dataset.value;
+
+  function showContent(value) {
+    if (!key || !value) return;
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    h2.innerHTML = key;
+    const p = document.createElement("p");
+    p.innerHTML = value;
+    div.appendChild(h2);
+    div.appendChild(p);
+    container.appendChild(div);
   
-  if (!key || !value) return;
-  const div = document.createElement("div");
-  const h2 = document.createElement("h2");
-  h2.innerHTML = key;
-  const p = document.createElement("p");
-  p.innerHTML = value;
-  div.appendChild(h2);
-  div.appendChild(p);
-  container.appendChild(div);
+  }
+
+  fetch("https://iguannalin.github.io/ctw/how-to/things.json").then((r) => r.json()).then((d) => {
+    showContent(d[key]);
+  })
 });
